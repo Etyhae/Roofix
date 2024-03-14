3aa90930fa40117609452b8507939478bbbb9a46
Etyhae <max.mand@yandex.ru>
2024-03-14 11:35:27 +0300

    feature: add real info

---
+++ assets/js/app.js
@@ -0,0 +1,6 @@
+// document.addEventListener("DOMContentLoaded", () => {
+//     const parts = document.querySelectorAll(".house_img");
+//     for (const item in parts) {
+//        item.addEventListener("")
+//     }
+// })
