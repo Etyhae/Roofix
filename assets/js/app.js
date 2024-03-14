24b079300c931c31ae570c19a9f94b33f820b786
Etyhae <max.mand@yandex.ru>
2024-03-14 11:57:53 +0300

    fix: house animation

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,10 @@
-// document.addEventListener("DOMContentLoaded", () => {
-//     const parts = document.querySelectorAll(".house_img");
-//     for (const item in parts) {
-//        item.addEventListener("")
-//     }
-// })
+document.addEventListener("DOMContentLoaded", () => {
+    const parts = document.querySelectorAll(".house_img");
+    for (let index = 0; index < parts.length; index++) {
+        const element = parts[index];
+        element.style.animationName = "slideIn";
+        element.style.animationDuration = `${parts.length - index - 1}s`;
+        element.style.animationDelay = "0s";
+        element.style.animationTimingFunction = "ease-out";
+    }
+})
